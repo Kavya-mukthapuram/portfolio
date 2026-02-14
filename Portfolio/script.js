d29261c19cd58715b28600508451fb017489337f
Kavya-mukthapuram <kavyamukthapuram672@gmail.com>
2026-02-14 17:12:06 +0530

    Add files via upload

---
+++ Portfolio/script.js
@@ -0,0 +1,55 @@
+const text="Frontend Developer  ";
+const speed=300;
+let index=0;
+function typeEffect(){
+    if(index<text.length){
+        document.getElementById("spa").textContent+=text.charAt(index);
+        index++;
+        setTimeout(typeEffect,speed);
+    }
+}
+window.onload=typeEffect;
+
+  const form = document.getElementById("contactForm");
+  const successMsg = document.getElementById("successMsg");
+
+  form.addEventListener("submit", async function (e) {
+    e.preventDefault(); // 🚫 stops Formspree page
+
+    const response = await fetch("https://formspree.io/f/xaqqwgqe", {
+      method: "POST",
+      body: new FormData(form),
+      headers: {
+        "Accept": "application/json"
+      }
+    });
+
+    if (response.ok) {
+      successMsg.style.display = "block";
+      form.reset();
+    } else {
+      alert("❌ Something went wrong. Please try again.");
+    }
+  });
+
+  // Animate headings
+  document.querySelectorAll("h1, h2, h3").forEach(el => {
+    el.setAttribute("data-aos", "fade-up");
+  });
+
+  // Animate paragraphs & list items
+  document.querySelectorAll("p, li").forEach(el => {
+    el.setAttribute("data-aos", "fade-up");
+  });
+
+  // Animate images
+  document.querySelectorAll("img").forEach(el => {
+    el.setAttribute("data-aos", "zoom-in");
+  });
+
+  AOS.init({
+    duration: 900,
+    once: true,
+    easing: "ease-out-cubic"
+  });
+
